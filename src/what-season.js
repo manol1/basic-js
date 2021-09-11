b563560eef2ed6b2b347d08ae8278fd69d43311a
manol1 <marsen1491@gmail.com>
2021-09-11 15:17:12 +0300

    feat: st-sum-digits

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    return 'Invalid date!';
+  }
+
+  if (typeof date.getMonth() !== 'number' || date.getMinutes() === new Date().getMinutes()) {
+    return 'Invalid date!';
+  }
+
+  if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
+    return 'winter';
+  } else if (date.getMonth() > 1 && date.getMonth() < 5) {
+    return 'spring';
+  } else if (date.getMonth() > 4 && date.getMonth() < 8) {
+    return 'summer';
+  } else if (date.getMonth() > 7 && date.getMonth() < 11) {
+    return 'autumn';
+}
 }

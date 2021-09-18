1e34382c372d8146b8e682ad5d35951910617228
manol1 <marsen1491@gmail.com>
2021-09-18 10:48:24 +0300

    feat: st-sort-by-height

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,37 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  let mass = [];
+  arr.forEach(function(el, i) {
+    if (el === -1) {
+      mass.push(i);
+    }
+  })
+  
+  let fil = arr.filter(el => el !== -1).sort(function (a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (a === b) {
+      return 0;
+    }
+    if (a < b) {
+      return -1;
+    }
+  })
+
+  let result = [];
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (mass.includes(i)) {
+      result.push(-1);
+    } else {
+      result.push(fil[j]);
+      j++;
+    }
+  }
+
+  return result
 }
